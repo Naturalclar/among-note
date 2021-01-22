ede5b1f849a3928adb2b930f40a3d736453ddde1
Jesse Katsumata <jesse.katsumata@gmail.com>
2021-01-22 22:59:28 +0900

    feat: add button check

--- src/compositions/PlayerState.svelte
+++ src/compositions/PlayerState.svelte
@@ -3,8 +3,37 @@
 
   export let color: PlayerColor = "Red";
   let isDead = false;
+  let usedButton = false;
   let memo = "";
 </script>
+
+<Section label={color}>
+  <div class="container">
+    <div class="imageContainer">
+      {#if isDead}
+        <img src="/images/dead/{color}.png" alt="Player {color} Dead" />
+      {:else}<img src="/images/players/{color}.png" alt="Player {color}" />{/if}
+    </div>
+    <div>
+      <div class="checkContainer">
+        <div>
+          <label for="button-{color}"> Button </label>
+          <input
+            id="button-{color}"
+            type="checkbox"
+            bind:checked={usedButton}
+          />
+        </div>
+        <div>
+          <label for="dead-{color}"> Dead </label>
+          <input id="dead-{color}" type="checkbox" bind:checked={isDead} />
+        </div>
+      </div>
+      <label for="memo-{color}"> Memo </label>
+      <textarea id="memo-{color}" bind:value={memo} />
+    </div>
+  </div>
+</Section>
 
 <style>
   .container {
@@ -29,21 +58,11 @@
   textarea {
     resize: none;
     width: 180px;
+    height: 200px;
+  }
+
+  .checkContainer {
+    display: flex;
+    justify-content: space-around;
   }
 </style>
-
-<Section label={color}>
-  <div class="container">
-    <div class="imageContainer">
-      {#if isDead}
-        <img src="/images/dead/{color}.png" alt="Player {color} Dead" />
-      {:else}<img src="/images/players/{color}.png" alt="Player {color}" />{/if}
-    </div>
-    <div>
-      <label for="dead-{color}"> Dead </label>
-      <input id="dead-{color}" type="checkbox" bind:checked={isDead} />
-      <label for="memo-{color}"> Memo </label>
-      <textarea id="memo-{color}" bind:value={memo} />
-    </div>
-  </div>
-</Section>
